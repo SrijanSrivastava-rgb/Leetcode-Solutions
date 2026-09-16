e74efd7483a19fe4447eb3d6877e8eca04436454
srijan1srivastava <ssrijan686@gmail.com>
2026-09-16 12:38:45 +0530

    Time: 2249 ms (17.51%), Space: 34.3 MB (94.34%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> mpp;
+    //     unordered_map<int, int> mpp;
 
-        mpp[0] = 1;
+    //     mpp[0] = 1;
 
-        int prefixSum = 0;
+    //     int prefixSum = 0;
+    //     int cnt = 0;
+
+    //     for(int i = 0; i<nums.size(); i++){
+    //         prefixSum += nums[i];
+
+    //     int required = prefixSum-k;
+
+    //     if(mpp.find(required) != mpp.end()){
+    //         cnt += mpp[required];
+    //     }
+    //     mpp[prefixSum]++;
+    // }
+    // return cnt;
+
+        // int n = nums.size();
+        // int cnt = 0;
+
+        // for(int i = 0; i<n; i++){
+        //     for(int j = i; j<n; j++){
+        //         int sum = 0;
+        //         for(int x = i; x<= j; x++){
+        //             sum += nums[x];
+        //         }
+        //         if(sum == k) cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        int n = nums.size();
         int cnt = 0;
 
-        for(int i = 0; i<nums.size(); i++){
-            prefixSum += nums[i];
-
-        int required = prefixSum-k;
-
-        if(mpp.find(required) != mpp.end()){
-            cnt += mpp[required];
+        for(int i = 0; i<n; i++){
+            int sum = 0;
+            for(int j = i; j<n; j++){
+                    sum += nums[j];
+                
+                if(sum == k) cnt++;
+            }
         }
-        mpp[prefixSum]++;
-    }
-    return cnt;
+        return cnt;
     }
 };
