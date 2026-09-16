b64d1d25d018902cec4f1e75940542329c4d8550
srijan1srivastava <ssrijan686@gmail.com>
2026-09-16 12:49:22 +0530

    Time: 2231 ms (17.72%), Space: 35.8 MB (80.51%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -34,15 +34,40 @@ public:
         // }
         // return cnt;
 
+        // int n = nums.size();
+        // int cnt = 0;
+
+        // for(int i = 0; i<n; i++){
+        //     int sum = 0;
+        //     for(int j = i; j<n; j++){
+        //             sum += nums[j];
+                
+        //         if(sum == k) cnt++;
+        //     }
+        // }
+        // return cnt;
+
         int n = nums.size();
         int cnt = 0;
 
-        for(int i = 0; i<n; i++){
-            int sum = 0;
-            for(int j = i; j<n; j++){
-                    sum += nums[j];
-                
-                if(sum == k) cnt++;
+        vector<int> prefix(n+1);
+        prefix[0] = nums[0];
+
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+
+        for(int i = 0; i<n ; i++){
+            for(int j=i; j<n; j++){
+                int sum = 0;
+                if(i == 0){
+                    sum = prefix[j];
+                }
+                else {
+                    sum = prefix[j] - prefix[i-1];
+                }
+
+                if(sum == k)cnt++;
             }
         }
         return cnt;
