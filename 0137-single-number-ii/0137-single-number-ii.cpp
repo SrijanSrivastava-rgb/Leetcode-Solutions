9c7d83686464c85b221159f3736d66c4c26f54ea
srijan1srivastava <ssrijan686@gmail.com>
2026-09-23 06:28:29 +0530

    Time: 0 ms (100%), Space: 13.4 MB (66.1%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int i=0; i<nums.size(); i++){
-            mp[nums[i]]++;
+        // unordered_map<int, int> mp;
+        // for(int i=0; i<nums.size(); i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto it : mp){
+        //     if(it.second == 1) return it.first;
+        // }
+        // return -1;
+
+        //Bitwise - optimal approach 
+        int ans = 0;
+        for(int bit=0; bit<32; bit++){
+            int cnt = 0;
+            for(int i=0; i<nums.size(); i++){
+                if(nums[i] & (1<<bit)){
+                    cnt++;
+                }
+            }
+            if(cnt % 3 == 1) {
+                ans = ans | (1<<bit);
+            }
         }
-        for(auto it : mp){
-            if(it.second == 1) return it.first;
-        }
-        return -1;
+        return ans;
     }
 };
